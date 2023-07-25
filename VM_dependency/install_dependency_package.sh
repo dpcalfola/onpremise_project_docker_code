8b563a8605daf3a01ea483e4e25e3372fee0c23b
Fola Flor <dpcalfola@gmail.com>
2023-07-25 11:56:28 +0900

    Fix > install_dependency_package.sh

--- VM_dependency/install_dependency_package.sh
+++ VM_dependency/install_dependency_package.sh
@@ -18,12 +18,12 @@ apt-get install openssh-server -y
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # install ca-certificates
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg -y
 
 # add docker's official GPG key
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo install -m 0755 -d /etc/apt/keyrings -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # set up the stable repository
@@ -33,8 +33,8 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install docker engine
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # checkup docker version
 docker version
