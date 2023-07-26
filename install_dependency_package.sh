275195d5eab9661d7149ce9797eefbcfc90fd504
Fola Flor <dpcalfola@gmail.com>
2023-07-26 16:45:52 +0900

    Fix > Modify docker install task to latest
    	File: install_dependency_package.sh
    	Modify docker inastall task to latest

--- install_dependency_package.sh
+++ install_dependency_package.sh
@@ -23,12 +23,12 @@ sudo apt-get install ca-certificates curl gnupg -y
 
 # add docker's official GPG key
 sudo install -m 0755 -d /etc/apt/keyrings -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # set up the stable repository
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
